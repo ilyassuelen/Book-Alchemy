5a06d8e2528b07b85d32861023311a091da0c513
Ilyas Sülen <ilyassulen@Mac.local>
2025-11-14 22:44:12 +0100

    Add authors/books routes, home page with covers and sorting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from data_models import db, Author, Book
@@ -13,6 +13,76 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 # Initialize Flask app for SQLAlchemy
 db.init_app(app)
 
-# Create tables
+
+@app.route("/add_author", methods=['GET', 'POST'])
+def add_author():
+    if request.method == "POST":
+        name = request.form.get("name")
+        birth_date = request.form.get("birthdate")
+        date_of_death = request.form.get("date_of_death")
+
+        # Create new author entry
+        new_author = Author(
+            name=name,
+            birth_date=birth_date,
+            date_of_death=date_of_death
+        )
+
+        db.session.add(new_author)
+        db.session.commit()
+
+        # Send success message back to the page
+        message = "Author added successfully!"
+        return render_template("add_author.html", message=message)
+
+    return render_template("add_author.html")
+
+
+@app.route("/add_book", methods=['GET', 'POST'])
+def add_book():
+    authors = Author.query.all()
+
+    if request.method == "POST":
+        title = request.form.get("book_title")
+        isbn = request.form.get("isbn")
+        publication_year = request.form.get("publication_year")
+        author_id = request.form.get("author_id")
+
+        # Create new book entry
+        new_book = Book(
+            title=title,
+            isbn=isbn,
+            publication_year=publication_year,
+            author_id=author_id
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        # Send success message back to the page
+        message = "Book added successfully!"
+        return render_template("add_book.html", authors=authors, message=message)
+
+    authors = Author.query.all()
+    return render_template("add_book.html", authors=authors)
+
+
+@app.route("/")
+def home():
+    sort = request.args.get("sort", "title")  # default: title
+
+    if sort == "author":
+        books = Book.query.join(Author).order_by(Author.name.asc()).all()
+    elif sort == "year":
+        books = Book.query.order_by(Book.publication_year.asc()).all()
+    else:
+        books = Book.query.order_by(Book.title.asc()).all()
+
+    return render_template("home.html", books=books, sort=sort)
+
+"""# Create tables
 with app.app_context():
-  db.create_all()
+  db.create_all()"""
+
+if __name__ == "__main__":
+    app.run()
