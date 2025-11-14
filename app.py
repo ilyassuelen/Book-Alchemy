da10bf2337ec2b6203bc8d1040ffcacaf87437ec
Ilyas Sülen <ilyassulen@Mac.local>
2025-11-14 20:58:37 +0100

    Configure Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from data_models import db, Author, Book
 
 # Create a Flask app instance
 app = Flask(__name__)
+
+# Configure SQLite database using absolute path
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+
+# Initialize Flask app for SQLAlchemy
+db.init_app(app)
